d18e145bc3f328ee4476f3883790de1fb500c7c9
sunghyun21 <jonnyjs2@addd.co.kr>
2025-10-23 20:39:24 -0700

    finished lab 2 webpage

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Get the element with the class "icon"
+let icon = document.getElementsByClassName("icon")[0];
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+  }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
